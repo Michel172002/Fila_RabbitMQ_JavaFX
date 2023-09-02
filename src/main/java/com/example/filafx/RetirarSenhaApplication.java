45c3b46682d7a426baace84bb880a89b7cb6ab48
erick <michel172002@gmail.com>
2023-09-01 22:44:41 -0300

    feat: Criado interface do painel para retirar as senhas

---
+++ src/main/java/com/example/filafx/RetirarSenhaApplication.java
@@ -0,0 +1,23 @@
+package com.example.filafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RetirarSenhaApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RetirarSenhaApplication.class.getResource("RetirarSenhaPainel.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
